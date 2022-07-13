325df3ca747c75c14f3778bfb9311979e87c17ed
Daniel Schwarz <daniel.schwarz@neuefische.de>
2022-07-13 14:38:05 +0200

    Initial commit

---
+++ src/main/java/com/example/cgnjava222gitbranches/CgnJava222GitBranchesApplication.java
@@ -0,0 +1,13 @@
+package com.example.cgnjava222gitbranches;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CgnJava222GitBranchesApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CgnJava222GitBranchesApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/cgnjava222gitbranches/CgnJava222GitBranchesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.cgnjava222gitbranches;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CgnJava222GitBranchesApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
